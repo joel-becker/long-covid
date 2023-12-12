aa82fab0e33676e7f513a3f95c9871bdd55c0ec0
joel-becker <joelhbkr@gmail.com>
2023-12-12 11:03:25 +0000

    clean

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import calculate_burden as cb
 import plot as plot
 import pandas as pd
 
+
 # Load data from CSV files
 prevalence_diff = prep.prepare_raw_data()
 data_daly = pd.read_csv('data/daly.csv')
+
 
 # Adjusting the prevalence data
 conservative_prevalence = ap.adjust_prevalence_data(prevalence_diff, method='conservative')
 moderate_prevalence = ap.adjust_prevalence_data(prevalence_diff, method='moderate')
 
+
 # Merging data
 conservative_prevalence = cb.merge_prevalence_severity(conservative_prevalence, data_daly)
 conservative_prevalence = cb.calculate_period_burden(conservative_prevalence)
+
 moderate_prevalence = cb.merge_prevalence_severity(moderate_prevalence, data_daly)
 moderate_prevalence = cb.calculate_period_burden(moderate_prevalence)
 
-# Define variables
-annual_cases = 19202639
 
+# Print total burden in different severity scenarios
 severity_proportions_mild = {'mild': 1, 'moderate': 0, 'severe': 0}
 severity_proportions_moderate = {'mild': 0, 'moderate': 1, 'severe': 0}
 severity_proportions_severe = {'mild': 0, 'moderate': 0, 'severe': 1}
@@ -36,6 +39,8 @@ severity_scenarios = {
     'Raddad': severity_proportions_raddad,
     'Robinson': severity_proportions_robinson,
 }
+
+annual_cases = 19202639
 
 for scenario_name, scenario in severity_scenarios.items():
     total_burden = cb.calculate_severity_adjusted_burden(conservative_prevalence, scenario, annual_cases)
